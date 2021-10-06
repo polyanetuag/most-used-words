364683e114dbbbdaa5aa85e7d6a52cea0d2cd282
Polyane Tuag <pollyportugues@hotmail.com>
2021-10-06 18:43:07 -0300

    creating form for subtitles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,15 @@
 <template>
   <v-container fluid>
+    <v-form>
+      <v-file-input 
+        label="Selecione uma ou mais legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        multiple
+        chips 
+        v-model="files"
+        @click:append-outer="processSubtitles" />
+    </v-form>
     <div class="pills">
       <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
     </div>
@@ -13,11 +23,17 @@ export default {
   components: { Pill },
   data: function () {
     return {
+      files: [],
       groupedWords: [
         {name: 'i' , amount: 1234 },
         {name: 'you' , amount: 530 },
         {name: 'he' , amount: 429 },
       ]
+    }
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files)
     }
   }
 }
